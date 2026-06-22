e04fe36f4f5caf0850b12a3ec83667c336ebe5f6
yogeshs <ys730292@gmail.com>
2026-06-22 18:49:55 +0530

    add

--- file-storage/express.js
+++ file-storage/express.js
@@ -1,6 +1,7 @@
 import express from "express";
+import fs from "node:fs/promises";
 const app = express();
 
-
+app.use(express.json());
 
 app.listen(8000, () => console.log(`server is listening on port 8080`));
